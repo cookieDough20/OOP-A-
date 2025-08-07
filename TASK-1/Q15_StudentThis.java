457faa91b2cf83b4459e4c997deede14ee37e283
cookieDough20 <2420030708@klh.edu.in>
2025-08-07 12:45:42 +0530

    folder changes

--- TASK-1/Q15_StudentThis.java
+++ TASK-1/Q15_StudentThis.java
@@ -13,11 +13,7 @@ public class Q15_StudentThis {
     }
 
     void display() {
-<<<<<<< HEAD
         System.out.println("ID: " + this.id + ", Namne: " + this.name + ", Course: " + this.course);
-=======
-        System.out.println("ID: " + this.id + ", Name: " + this.name + ", Course: " + this.course);
->>>>>>> c164e98ec9747bc203084566aa6aaa7a932b2d07
     }
 
     public static void main(String[] args) {
