585b0378e1cffce47dc0ea7e5f5432a41c488014
cookieDough20 <2420030708@klh.edu.in>
2025-08-07 09:06:04 +0530

    Updated TASK-1 with new files

--- TASK-1/Q12_FinalError.java
+++
@@ -1,7 +0,0 @@
-public class Q12_FinalError {
-    public static void main(String[] args) {
-        final int x = 10;
-        // x = 20; // ❌ Uncommenting this line will cause a compile-time error
-        System.out.println("x = " + x);
-    }
-}
